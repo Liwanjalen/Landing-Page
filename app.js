4fc23e39104c0cdf3abcab5509fbd23a5953708c
Jalen Liwan <liwanjalenzki@gmail.com>
2023-07-20 19:08:35 +0800

    final landing page

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("working");
+$("button").click(function (e) {
+  console.log(e.target.value);
+  $("html,body").animate(
+    {
+      scrollTop: $(e.target.value).offset().top,
+    },
+    "slow"
+  );
+});
